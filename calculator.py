c37f108dd0eb799cc3eb18d54c9d587fa66c346c
Temirlan <temirlan178@gmail.com>
2020-10-12 00:56:31 +0600

    divide function

--- calculator.py
+++ calculator.py
@@ -6,6 +6,10 @@ def add(x, y):
 def subtract(x, y):
     return x - y
 
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
