0adeb4069d71ea5c28fd68c53b4bc8b129932aa2
Vlada971 <vladaboltenko@gmail.com>
2020-10-12 01:01:37 +0600

    Added multiplication func

--- calculator.py
+++ calculator.py
@@ -6,6 +6,10 @@ def add(x, y):
 def subtract(x, y):
     return x - y
 
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
 # This function divides two numbers
 def divide(x, y):
     return x / y
